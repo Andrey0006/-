52a30e850df38cdaad268be98e171cee408278ad
Andrey0006 <51323102+Andrey0006@users.noreply.github.com>
2019-06-04 11:15:06 +0300

    10.Секунды

--- 10.Секунды.py
+++ 10.Секунды.py
@@ -1,3 +1,12 @@
+import datetime
+
+def printTimeStamp(name):
+
+  print('Автор програми: ' + name)
+
+  print('Час компіляції: ' + str(datetime.datetime.now()))
+
+
 print('Введіть проміжок часу:\n\n ')
 
 d = input('День: ')
@@ -13,6 +22,7 @@ mhdc = int(mc) + int(hc) + int(dc) + int(c)
 
 print(mhdc)
 
+printTimeStamp('\nОсередько Андрій, Ваня Жаботинський\n')
 
 
 input('\n')
