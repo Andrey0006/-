5a3f347b6fa651b709205f708a65311f893a51cf
Andrey0006 <51323102+Andrey0006@users.noreply.github.com>
2019-06-04 11:25:49 +0300

    relise v1

--- 9.СмвФуты.py
+++ 9.СмвФуты.py
@@ -1,3 +1,12 @@
+import datetime
+
+def printTimeStamp(name):
+
+  print('Автор програми: ' + name)
+
+  print('Час компіляції: ' + str(datetime.datetime.now()))
+
+
 a = input('Введіть велbчену в сантиметрах: ')
 
 D = float(a) * 0.39
@@ -7,5 +16,8 @@ F = D / 12
 print('Дюйми: ' + str(D))
 
 print('Фути' + str(F))
+
+printTimeStamp('\nОсередько Андрій, Ваня Жаботинський\n')
+
 
 input('\n')
